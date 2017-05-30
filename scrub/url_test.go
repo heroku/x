ef3ce993ac32d1813523f8c2638046a9692521ef
Christine Dodrill <cdodrill@heroku.com>
2017-05-30 08:59:18 -0700

    scrub: test URL scrubbing for all URL values too

--- scrub/url_test.go
+++ scrub/url_test.go
@@ -2,6 +2,7 @@ package scrub
 
 import (
 	"net/url"
+	"strings"
 	"testing"
 )
 
@@ -66,5 +67,26 @@ func TestURLShouldntScrub(t *testing.T) {
 
 	if val := scq.Get(param); val != eValue {
 		t.Fatalf("%s: want: %q, got: %q", param, eValue, val)
+	}
+}
+
+func TestURLinURL(t *testing.T) {
+	const secret = "hunter2" // http://bash.org/?244321
+	for name := range RestrictedParams {
+		t.Run(name, func(tt *testing.T) {
+			u, q := mustParseURL(t, "https://doesntlooklikestarsto.me/login")
+			q.Set(name, secret)
+			u.RawQuery = q.Encode()
+
+			ou, oq := mustParseURL(t, "functor://applicative/monad")
+			oq.Set("redirect_url", u.String())
+			ou.RawQuery = oq.Encode()
+
+			sc := URL(ou)
+			if strings.Contains(sc.String(), secret) {
+				tt.Fatalf("found parameter that should be scrubbed: input: %q, output: %q", ou.String(), sc.String())
+			}
+			t.Log(sc.String())
+		})
 	}
 }
