89845a7d0b1adc2ea21772162f5b22147066ec8a
Christine Dodrill <cdodrill@heroku.com>
2017-06-16 13:35:50 -0700

    cache: clean up encoder/decoder, add documentation and tests

--- cache/serialize.go
+++ cache/serialize.go
@@ -2,18 +2,21 @@ package cache
 
 import "errors"
 
-var errNotString = errors.New("StringEncoder: Unable to Encode non-string")
+// ErrNotString is returned when StringEncoder is fed an interface{} that is not
+// underlying type string.
+var ErrNotString = errors.New("StringEncoder: Unable to Encode non-string")
 
-// StringEncoder ...
+// StringEncoder encodes a string to a byte slice. This will fail if it is not
+// passed a string.
 func StringEncoder(v interface{}) ([]byte, error) {
 	str, ok := v.(string)
 	if !ok {
-		return nil, errNotString
+		return nil, ErrNotString
 	}
 	return []byte(str), nil
 }
 
-// StringDecoder ...
+// StringDecoder converts a byte slice into a string.
 func StringDecoder(buf []byte) (interface{}, error) {
 	return string(buf), nil
 }
