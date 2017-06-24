1a713286d1ed61825ae7ad054291927c7bf05f93
Edward Muller <edwardam@interlix.com>
2017-06-23 18:52:28 -0700

    librato: restructure the tests

--- go-kit/metrics/provider/librato/librato_test.go
+++ go-kit/metrics/provider/librato/librato_test.go
@@ -4,75 +4,98 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"math"
 	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"os"
-	"sort"
 	"testing"
 	"time"
-
-	"github.com/go-kit/kit/metrics/generic"
 )
 
-func TestLibratoReportWithRequestDebugging(t *testing.T) {
-	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusBadRequest)
-	}))
-	defer srv.Close()
+var (
+	doesntmatter = time.Hour
+)
 
-	u, err := url.Parse(srv.URL)
+func ExampleNew() {
+	start := time.Now()
+	u, err := url.Parse(DefaultURL)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
+	u.User = url.UserPassword("libratoUser", "libratoPassword/Token")
 
-	u.User = url.UserPassword("librato", "secret123")
+	errHandler := func(err error) {
+		log.Println(err)
+	}
+	p := New(u, 20*time.Second, WithErrorHandler(errHandler))
+	c := p.NewCounter("i.am.a.counter")
+	h := p.NewHistogram("i.am.a.histogram", DefaultBucketCount)
+	g := p.NewGauge("i.am.a.gauge")
 
-	t.Run("request debugging", func(t *testing.T) {
-		p := Provider{
-			done:             make(chan struct{}),
-			requestDebugging: true,
-		}
+	// Pretend applicaion logic....
+	c.Add(1)
+	h.Observe(time.Now().Sub(start).Seconds()) // how long did it take the program to get here.
+	g.Set(1000)
+	// /Pretend
 
-		p.NewCounter("foo").Add(1)
+	// block until we report one final time
+	p.Stop()
+}
 
-		err = p.report(u, time.Minute)
-		if err == nil {
-			t.Fatal("got nil err wanted something")
-		}
+func TestLibratoReportRequestDebugging(t *testing.T) {
+	for _, debug := range []bool{true, false} {
+		t.Run(fmt.Sprintf("%t", debug), func(t *testing.T) {
+			t.Parallel()
+			srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusBadRequest)
+			}))
+			defer srv.Close()
+			u, err := url.Parse(srv.URL)
+			if err != nil {
+				t.Fatal(err)
+			}
+			p := New(u, doesntmatter, func(p *Provider) { p.requestDebugging = debug })
+			p.Stop()
+			p.NewCounter("foo").Add(1) // need at least one metric in order to report
+			err = p.(*Provider).report(u, doesntmatter)
+			if err == nil {
+				t.Fatal("expected error, got nil")
+			}
 
-		e, ok := err.(Error)
-		if !ok {
-			t.Fatalf("got %T want Error", err)
-		}
+			e, ok := err.(Error)
+			if !ok {
+				t.Fatalf("expected an Error, got %T: %q", err, err.Error())
+			}
 
-		checkRequestDebugging(t, u, e)
+			req := e.Request()
+			if debug {
+				if req.URL.String() != u.String() {
+					t.Fatalf("expected %s, got %s", u.String(), req.URL.String())
+				}
+				eContentType := "application/json"
+				if v := req.Header.Get("Content-Type"); v != eContentType {
+					t.Fatalf("expected %q, got %q", eContentType, v)
+				}
+				buf, _ := ioutil.ReadAll(req.Body)
 
-	})
+				var payload map[string]interface{}
+				if err := json.Unmarshal(buf, &payload); err != nil {
+					t.Fatal("unexpected error", err)
+				}
+				if len(payload) == 0 {
+					t.Fatal("expected payload with data, got empty payload")
+				}
+			} else {
+				if req != nil {
+					t.Errorf("expected no request, got %#v", req)
+				}
+			}
 
-	t.Run("no request debugging", func(t *testing.T) {
-		p := Provider{
-			done:             make(chan struct{}),
-			requestDebugging: false,
-		}
-
-		p.NewCounter("foo").Add(1)
-
-		err = p.report(u, time.Minute)
-		if err == nil {
-			t.Fatal("got nil err wanted something")
-		}
-
-		e, ok := err.(Error)
-		if !ok {
-			t.Fatalf("got %T want Error", err)
-		}
-
-		checkNoRequestDebugging(t, u, e)
-	})
-
+		})
+	}
 }
 
 func TestLibratoRetriesWithErrors(t *testing.T) {
@@ -88,31 +111,25 @@ func TestLibratoRetriesWithErrors(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	u.User = url.UserPassword("librato", "secret123")
-
 	var totalErrors, temporaryErrors, finalErrors int
 	expectedRetries := 3
-	p := Provider{
-		done:             make(chan struct{}),
-		requestDebugging: true,
-		numRetries:       expectedRetries,
-		errorHandler: func(err error) {
-			totalErrors++
-			type temporary interface {
-				Temporary() bool
+	errHandler := func(err error) {
+		totalErrors++
+		type temporary interface {
+			Temporary() bool
+		}
+		if terr, ok := err.(temporary); ok {
+			if terr.Temporary() {
+				temporaryErrors++
+			} else {
+				finalErrors++
 			}
-			if terr, ok := err.(temporary); ok {
-				if terr.Temporary() {
-					temporaryErrors++
-				} else {
-					finalErrors++
-				}
-			}
-		},
+		}
 	}
-
-	p.NewCounter("foo").Add(1)
-	p.reportWithRetry(u, time.Minute)
+	p := New(u, doesntmatter, WithErrorHandler(errHandler), WithRetries(expectedRetries), WithRequestDebugging()).(*Provider)
+	p.Stop()
+	p.NewCounter("foo").Add(1) // need at least one metric in order to report
+	p.reportWithRetry(u, doesntmatter)
 
 	if totalErrors != expectedRetries {
 		t.Errorf("expected %d total errors, got %d", expectedRetries, totalErrors)
@@ -133,34 +150,6 @@ func TestLibratoRetriesWithErrors(t *testing.T) {
 	}
 }
 
-func checkNoRequestDebugging(t testing.TB, u *url.URL, e Error) {
-	if e.Request() != nil {
-		t.Fatalf("got request non nil for request debugging off, expected nil")
-	}
-}
-
-func checkRequestDebugging(t testing.TB, u *url.URL, e Error) {
-	req := e.Request()
-	if req.URL.String() != u.String() {
-		t.Fatalf("got url %s want %s", req.URL, u)
-	}
-
-	if v := req.Header.Get("Content-Type"); v != "application/json" {
-		t.Fatalf("got content type %v want %v", v, "application/json")
-	}
-
-	buf, _ := ioutil.ReadAll(req.Body)
-
-	var payload map[string]interface{}
-	if err := json.Unmarshal(buf, &payload); err != nil {
-		t.Fatal(err)
-	}
-
-	if len(payload) == 0 {
-		t.Fatal("got no payload wanted something")
-	}
-}
-
 func TestLibratoSingleReport(t *testing.T) {
 	user := os.Getenv("LIBRATO_TEST_USER")
 	pwd := os.Getenv("LIBRATO_TEST_PWD")
@@ -170,7 +159,7 @@ func TestLibratoSingleReport(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	u, err := url.Parse(DefaultURL)
 	if err != nil {
-		t.Fatalf("got %q, expected nil", err)
+		t.Fatalf("expected nil, got %q", err)
 	}
 	u.User = url.UserPassword(user, pwd)
 
@@ -187,7 +176,7 @@ func TestLibratoSingleReport(t *testing.T) {
 
 	// Call the reporter explicitly
 	if err := p.report(u, 10*time.Second); err != nil {
-		t.Fatalf("got %q, expected nil", err)
+		t.Fatalf("expected nil, got %q", err)
 	}
 }
 
@@ -200,12 +189,12 @@ func TestLibratoReport(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	u, err := url.Parse(DefaultURL)
 	if err != nil {
-		t.Fatalf("got %q, expected nil", err)
+		t.Fatalf("expected nil, got %q", err)
 	}
 	u.User = url.UserPassword(user, pwd)
 
 	errHandler := func(err error) {
-		t.Errorf("got %q but didn't expect any errors", err)
+		t.Errorf("expected nil, got %q", err)
 	}
 
 	p := New(u, time.Second, WithSource("test.source"), WithErrorHandler(errHandler))
@@ -231,40 +220,6 @@ func TestLibratoReport(t *testing.T) {
 	<-done
 }
 
-func gaugeExpectations(t *testing.T, gJSON []byte, eJSON, eName string, eCount int64, ePeriod, eSum, eMin, eMax, eSumSq float64) {
-	if string(gJSON) != eJSON {
-		t.Errorf("got %q\nexpected %q", gJSON, eJSON)
-	}
-
-	var tg gauge
-	err := json.Unmarshal(gJSON, &tg)
-	if err != nil {
-		t.Fatalf("got %q, expected nil", err)
-	}
-
-	if tg.Name != eName {
-		t.Errorf("got %q, expected %q", tg.Name, eName)
-	}
-	if tg.Count != eCount {
-		t.Errorf("got %d, expected %d", tg.Count, eCount)
-	}
-	if tg.Period != ePeriod {
-		t.Errorf("got %f, expected %f", tg.Period, ePeriod)
-	}
-	if tg.Sum != eSum {
-		t.Errorf("got %f, expected %f", tg.Sum, eSum)
-	}
-	if tg.Min != eMin {
-		t.Errorf("got %f, expected %f", tg.Min, eMin)
-	}
-	if tg.Max != eMax {
-		t.Errorf("got %f, expected %f", tg.Max, eMax)
-	}
-	if tg.SumSq != eSumSq {
-		t.Errorf("got %f, expected %f", tg.SumSq, eSumSq)
-	}
-}
-
 func TestLibratoHistogramJSONMarshalers(t *testing.T) {
 	h := Histogram{name: "test.histogram", buckets: DefaultBucketCount, percentilePrefix: ".p"}
 	h.reset()
@@ -274,195 +229,127 @@ func TestLibratoHistogramJSONMarshalers(t *testing.T) {
 	ePeriod := 1.0
 	d := h.measures(ePeriod)
 	if len(d) != 4 {
-		t.Fatalf("got %d, expected length to be 4", len(d))
+		t.Fatalf("expected length of parts to be 4, got %d", len(d))
 	}
+
 	p1, err := json.Marshal(d[0])
 	if err != nil {
-		t.Fatalf("got %q, expected nil", err)
+		t.Fatal("unexpected error unmarshaling", err)
 	}
-	ej := `{"name":"test.histogram","period":1,"count":3,"sum":260,"min":10,"max":150,"sum_squares":32600}`
-	if string(p1) != ej {
-		t.Errorf("got %q\nexpected %q", p1, ej)
-	}
-
-	// Double check our expectations.
-	var tg gauge
-	err = json.Unmarshal(p1, &tg)
-	if err != nil {
-		t.Fatalf("got %q, expected nil", err)
-	}
-	if tg.Period != ePeriod {
-		t.Errorf("period: got %f, expected %f", tg.Period, ePeriod)
-	}
-	en := "test.histogram"
-	if tg.Name != en {
-		t.Errorf("name: got %q, expected %q", tg.Name, en)
-	}
-	ec := int64(3)
-	if tg.Count != ec {
-		t.Errorf("count: got %d, expected %d", tg.Count, ec)
-	}
-	es := 260.0
-	if math.Float64bits(tg.Sum) != math.Float64bits(es) {
-		t.Errorf("sum: got %f, expected %f", tg.Sum, es)
-	}
-	ess := 32600.0
-	if math.Float64bits(tg.SumSq) != math.Float64bits(ess) {
-		t.Errorf("sum_squares: got %f, expected %f", tg.SumSq, ess)
-	}
-	em := 10.0
-	if math.Float64bits(tg.Min) != math.Float64bits(em) {
-		t.Errorf("min: got %f, expected %f", tg.Min, em)
-	}
-	em = 150.0
-	if math.Float64bits(tg.Max) != math.Float64bits(em) {
-		t.Errorf("max: got %f, expected %f", tg.Max, em)
-	}
-
 	p99, err := json.Marshal(d[1])
 	if err != nil {
-		t.Fatalf("got %q, expected nil", err)
+		t.Fatal("unexpected error unmarshaling", err)
 	}
-	ep99 := `{"name":"test.histogram.p99","period":1,"count":1,"sum":150,"min":150,"max":150,"sum_squares":22500}`
-	ep99n := "test.histogram.p99"
-	ep99c := int64(1)
-	ep99min := 150.0
-	ep99max := 150.0
-	ep99sum := 150.0
-	ep99sumsq := ep99sum * ep99sum
-	gaugeExpectations(t, p99, ep99, ep99n, ep99c, ePeriod, ep99sum, ep99min, ep99max, ep99sumsq)
-
 	p95, err := json.Marshal(d[2])
 	if err != nil {
-		t.Fatalf("got %q, expected nil", err)
+		t.Fatal("unexpected error unmarshaling", err)
 	}
-	ep95 := `{"name":"test.histogram.p95","period":1,"count":1,"sum":150,"min":150,"max":150,"sum_squares":22500}`
-	ep95n := "test.histogram.p95"
-	ep95c := int64(1)
-	ep95min := 150.0
-	ep95max := 150.0
-	ep95sum := 150.0
-	ep95sumsq := ep95sum * ep95sum
-	gaugeExpectations(t, p95, ep95, ep95n, ep95c, ePeriod, ep95sum, ep95min, ep95max, ep95sumsq)
-
 	p50, err := json.Marshal(d[3])
 	if err != nil {
-		t.Fatalf("got %q, expected nil", err)
+		t.Fatal("unexpected error unmarshaling", err)
 	}
-	ep50 := `{"name":"test.histogram.p50","period":1,"count":1,"sum":100,"min":100,"max":100,"sum_squares":10000}`
-	ep50n := "test.histogram.p50"
-	ep50c := int64(1)
-	ep50min := 100.00
-	ep50max := 100.0
-	ep50sum := 100.00
-	ep50sumsq := ep50sum * ep50sum
-	gaugeExpectations(t, p50, ep50, ep50n, ep50c, ePeriod, ep50sum, ep50min, ep50max, ep50sumsq)
-}
 
-type testcase struct {
-	name        string
-	values      []float64
-	eCount      int64
-	eSum        float64
-	eSumSquares float64
-	eMin        float64
-	eMax        float64
-}
-
-func generateHistogramTestData(c, max int) testcase {
-	rand.Seed(time.Now().UnixNano())
-	var tc testcase
-	tc.name = fmt.Sprintf("test %d %d", c, max)
-	tc.eCount = int64(c)
-	tc.eMin = 0
-	values := make([]float64, 0, c)
-	for i := 0; i < c; i++ {
-		v := float64(rand.Intn(max))
-		values = append(values, v)
-		tc.values = append(tc.values, v)
-		tc.eSum += v
-		tc.eSumSquares += v * v
-		if v < tc.eMin || tc.eMin == 0 {
-			tc.eMin = v
-		}
-		if v > tc.eMax {
-			tc.eMax = v
-		}
+	cases := []struct {
+		eRaw, eName              string
+		eCount                   int64
+		eMin, eMax, eSum, eSumSq float64
+		input                    []byte
+	}{
+		{
+			eRaw:   `{"name":"test.histogram","period":1,"count":3,"sum":260,"min":10,"max":150,"sum_squares":32600}`,
+			eName:  "test.histogram",
+			eCount: 3, eMin: 10, eMax: 150, eSum: 260, eSumSq: 32600,
+			input: p1,
+		},
+		{
+			eRaw:   `{"name":"test.histogram.p99","period":1,"count":1,"sum":150,"min":150,"max":150,"sum_squares":22500}`,
+			eName:  "test.histogram.p99",
+			eCount: 1, eMin: 150, eMax: 150, eSum: 150, eSumSq: 22500,
+			input: p99,
+		},
+		{
+			eRaw:   `{"name":"test.histogram.p95","period":1,"count":1,"sum":150,"min":150,"max":150,"sum_squares":22500}`,
+			eName:  "test.histogram.p95",
+			eCount: 1, eMin: 150, eMax: 150, eSum: 150, eSumSq: 22500,
+			input: p95,
+		},
+		{
+			eRaw:   `{"name":"test.histogram.p50","period":1,"count":1,"sum":100,"min":100,"max":100,"sum_squares":10000}`,
+			eName:  "test.histogram.p50",
+			eCount: 1, eMin: 100, eMax: 100, eSum: 100, eSumSq: 10000,
+			input: p50,
+		},
 	}
-	sort.Float64s(values)
-	return tc
-}
 
-func TestHistogram(t *testing.T) {
-	var p Provider
-	for _, tc := range []testcase{
-		generateHistogramTestData(10, 5*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(100, 5*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(1000, 5*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(10000, 5*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(100000, 5*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(10, 10*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(100, 10*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(1000, 10*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(10000, 10*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(100000, 10*int(time.Second/time.Microsecond)),
-		generateHistogramTestData(10, int(time.Hour/time.Microsecond)),
-		generateHistogramTestData(100, int(time.Hour/time.Microsecond)),
-		generateHistogramTestData(1000, int(time.Hour/time.Microsecond)),
-		generateHistogramTestData(10000, int(time.Hour/time.Microsecond)),
-		generateHistogramTestData(100000, int(time.Hour/time.Microsecond)),
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			h := p.NewHistogram(tc.name, DefaultBucketCount)
-			for _, v := range tc.values {
-				h.Observe(v)
+	for _, tc := range cases {
+		t.Run(tc.eName, func(t *testing.T) {
+			t.Parallel()
+			if string(tc.input) != tc.eRaw {
+				t.Errorf("expected %q\ngot %q", tc.eRaw, tc.input)
 			}
-			lh, ok := h.(*Histogram)
-			if !ok {
-				t.Fatal("Could not convert to *Histogram")
+
+			var tg gauge
+			err := json.Unmarshal(tc.input, &tg)
+			if err != nil {
+				t.Fatal("unexpected error unmarshalling", err)
 			}
-			if c := lh.Count(); c != tc.eCount {
-				t.Errorf("go Count() (%d), but expected (%d)", c, tc.eCount)
+
+			if tg.Name != tc.eName {
+				t.Errorf("expected %q, got %q", tc.eName, tg.Name)
 			}
-			if s := lh.Sum(); s != tc.eSum {
-				t.Errorf("got Sum() (%f), but expected (%f)", s, tc.eSum)
+			if tg.Count != tc.eCount {
+				t.Errorf("expected %d, got %d", tc.eCount, tg.Count)
 			}
-			if ss := lh.SumSq(); ss != tc.eSumSquares {
-				t.Errorf("got SumSq() squares (%f), but expected (%f)", ss, tc.eSum)
+			if tg.Period != ePeriod {
+				t.Errorf("expected %f, got %f", ePeriod, tg.Period)
 			}
-			if m := lh.Min(); m != tc.eMin {
-				t.Errorf("got Min() (%f), but expected (%f)", m, tc.eMin)
+			if math.Float64bits(tg.Sum) != math.Float64bits(tc.eSum) {
+				t.Errorf("expected %f, got %f", tc.eSum, tg.Sum)
 			}
-			if m := lh.Max(); m != tc.eMax {
-				t.Errorf("got Max() (%f), but expecte (%f)", m, tc.eMax)
+			if math.Float64bits(tg.Min) != math.Float64bits(tc.eMin) {
+				t.Errorf("expected %f, got %f", tc.eMin, tg.Min)
 			}
+			if math.Float64bits(tg.Max) != math.Float64bits(tc.eMax) {
+				t.Errorf("expected %f, got %f", tc.eMin, tg.Max)
+			}
+			if math.Float64bits(tg.SumSq) != math.Float64bits(tc.eSumSq) {
+				t.Errorf("expected %f, got %f", tc.eSumSq, tg.SumSq)
+			}
+
 		})
 	}
 }
 
 func TestWithResetCounters(t *testing.T) {
-	u, err := url.Parse(DefaultURL)
-	if err != nil {
-		t.Fatalf("got %q, expected nil", err)
-	}
+	for _, reset := range []bool{true, false} {
+		t.Run(fmt.Sprintf("%t", reset), func(t *testing.T) {
+			t.Parallel()
+			srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+			}))
+			defer srv.Close()
+			u, err := url.Parse(srv.URL)
+			if err != nil {
+				t.Fatal(err)
+			}
+			p := New(u, doesntmatter, func(p *Provider) { p.resetCounters = reset })
+			p.Stop()
+			foo := p.NewCounter("foo")
+			foo.Add(1)
+			p.(*Provider).report(u, doesntmatter)
 
-	cases := []struct {
-		resetCounters bool
-		added         float64
-		want          float64
-	}{
-		{true, 1, 0},
-		{false, 1, 1},
-	}
-
-	for _, test := range cases {
-		p := &Provider{resetCounters: test.resetCounters}
-		foo := p.NewCounter("foo")
-		foo.Add(test.added)
-		p.report(u, time.Second)
-
-		if v := foo.(*generic.Counter).Value(); v != test.want {
-			t.Fatalf("got %v want %v", v, test.want)
-		}
+			var expected float64
+			if reset {
+				expected = 0
+			} else {
+				expected = 1
+			}
+			type valuer interface {
+				Value() float64
+			}
+			if v := foo.(valuer).Value(); v != expected {
+				t.Errorf("expected %f, got %f", expected, v)
+			}
+		})
 	}
 }
