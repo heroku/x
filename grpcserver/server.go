e1f66d0f794161b40ab34b9323fa93181fe6dbe9
Bernerd Schaefer <bj.schaefer@gmail.com>
2017-07-05 16:58:43 -0700

    Update to logrus 1.0 with new import name (#738)
    logrus has been renamed from Sirupsen/logrus to sirupsen/logrus. We need
    to use the new name to be able to continue to use it going forward, as
    well as to bring in other community packages which have already been
    updated.
    
    This updates logrus and rollrus, and changes everywhere that was
    Sirupsen/logrus to sirupsen/logrus.

--- grpcserver/server.go
+++ grpcserver/server.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"net"
 
-	log "github.com/Sirupsen/logrus"
 	proxyproto "github.com/armon/go-proxyproto"
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
 	"github.com/heroku/cedar/lib/grpc/grpcclient"
 	"github.com/heroku/cedar/lib/grpc/grpcmetrics"
 	"github.com/heroku/cedar/lib/grpc/panichandler"
@@ -14,7 +14,10 @@ import (
 	"github.com/heroku/x/go-kit/metrics"
 	"github.com/mwitkow/go-grpc-middleware"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	xcontext "golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	healthgrpc "google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
@@ -72,6 +75,7 @@ func RunStandardServer(logger log.FieldLogger, p metrics.Provider, port int, ser
 		return err
 	}
 
+	// TODO: use StandardOptions() + grpc.Creds
 	uph := panichandler.LoggingUnaryPanicHandler(logger)
 	sph := panichandler.LoggingStreamPanicHandler(logger)
 
@@ -102,4 +106,48 @@ func RunStandardServer(logger log.FieldLogger, p metrics.Provider, port int, ser
 	}).Print()
 
 	return srv.Serve(proxyprotoLn)
+}
+
+// StandardOptions return a list of standard server options to initialize
+// servers.
+func StandardOptions(l *log.Entry, p metrics.Provider) []grpc.ServerOption {
+	logOpts := []grpc_logrus.Option{
+		grpc_logrus.WithCodes(func(err error) codes.Code {
+			return grpc.Code(errors.Cause(err))
+		}),
+	}
+
+	return []grpc.ServerOption{
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			panichandler.LoggingUnaryPanicHandler(l),
+			grpcmetrics.NewUnaryServerInterceptor(p), // report metrics on unwrapped errors
+			unaryServerErrorUnwrapper,                // unwrap after we've logged
+			grpc_logrus.UnaryServerInterceptor(l, logOpts...),
+		)),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			panichandler.LoggingStreamPanicHandler(l),
+			grpc_logrus.StreamServerInterceptor(l, logOpts...),
+			grpcmetrics.NewStreamServerInterceptor(p),
+		)),
+	}
+}
+
+// NewStandardInProcess starts a new in-proces gRPC server with the standard
+// middleware and returns the server and a valid connection.
+func NewStandardInProcess(l *log.Entry, p metrics.Provider) (*grpc.Server, *grpc.ClientConn, error) {
+	srv, err := NewInProcess("local", StandardOptions(l, p)...)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return srv.Server, grpcclient.Conn("local"), nil
+}
+
+// unaryServerErrorUnwrapper removes errors.Wrap annotations from errors so
+// gRPC status codes are correctly returned to interceptors and clients later
+// in the chain.
+func unaryServerErrorUnwrapper(ctx xcontext.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (_ interface{}, err error) {
+	res, err := handler(ctx, req)
+	return res, errors.Cause(err)
 }
--- panichandler/panichandler.go
+++ panichandler/panichandler.go
@@ -1,8 +1,8 @@
 package panichandler
 
 import (
-	log "github.com/Sirupsen/logrus"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
