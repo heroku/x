db4a66aa8d07b3331ed1454cc2b55df502c70e5a
Christine Dodrill <cdodrill@salesforce.com>
2018-03-13 15:26:39 -0700

    hredis: add package commentary

---
+++ hredis/doc.go
@@ -0,0 +1,9 @@
+/* Copyright (c) 2018 Salesforce
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+// Package hredis is a small set of utilities and useful functions for dealing
+// with Redis on Heroku.
+package hredis
