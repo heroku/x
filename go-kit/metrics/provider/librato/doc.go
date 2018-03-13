71a1b6180ab59c0d002b7ba32f986124572ab4de
Christine Dodrill <cdodrill@salesforce.com>
2018-03-13 15:20:31 -0700

    go-kit/metrics/provider/librato: package commentary

---
+++ go-kit/metrics/provider/librato/doc.go
@@ -0,0 +1,10 @@
+/* Copyright (c) 2018 Salesforce
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+// Package librato is a small wrapper around Librato's API for submitting metrics.
+//
+// This satisfies the go-kit metrics Provider type.
+package librato
