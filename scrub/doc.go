268fa50b6888adcb9ab3e6ae07fab2c5658e6919
Christine Dodrill <cdodrill@salesforce.com>
2018-03-13 15:51:23 -0700

    scrub: license comments

--- scrub/doc.go
+++ scrub/doc.go
@@ -1,3 +1,9 @@
+/* Copyright (c) 2018 Salesforce
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 // Package scrub defines helpers for removing sensitive data from HTTP headers
 // and URLs to make them safe for logging.
 package scrub
