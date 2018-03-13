f14dc8a8e6e841c461521032b594628f4ea892d6
Christine Dodrill <cdodrill@salesforce.com>
2018-03-13 12:30:59 -0700

    hcontext: package commentary

---
+++ hcontext/doc.go
@@ -0,0 +1,6 @@
+// Package hcontext is a small set of helpers and utilities for managing values
+// commonly kept in contexts in Heroku Go code.
+//
+// Each additional bit of data stored in a context should have its own files
+// in this package.
+package hcontext
