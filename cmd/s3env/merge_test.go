c985e18d960445b755cbe3848fdb01aff834adbc
Bryan Vanderhoof <bvanderhoof@heroku.com>
2018-06-22 21:25:24 -0500

    cmd/s3env: use correct slice length to avoid dropping config vars

--- cmd/s3env/merge_test.go
+++ cmd/s3env/merge_test.go
@@ -5,11 +5,14 @@ import (
 )
 
 func TestMerge(t *testing.T) {
-	r := merge(map[string]string{"A": "1"}, []string{"B=2"})
+	r := merge(map[string]string{"A": "1"}, []string{"B=2", "C=3"})
 	if r[0] != "B=2" {
 		t.Errorf("expected B=2, got %q", r[0])
 	}
-	if r[1] != "A=1" {
-		t.Errorf("expected A=1, got %q", r[1])
+	if r[1] != "C=3" {
+		t.Errorf("expected C=3, got %q", r[1])
+	}
+	if r[2] != "A=1" {
+		t.Errorf("expected A=1, got %q", r[2])
 	}
 }
