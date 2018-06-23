2d858ab76572bda11e5557545de575e42f83e854
Cyril David <cyx@cyx.is>
2018-06-22 19:30:00 -0700

    BUGFIX: s3env os.environ dropping things

--- cmd/s3env/merge_test.go
+++ cmd/s3env/merge_test.go
@@ -13,3 +13,15 @@ func TestMerge(t *testing.T) {
 		t.Errorf("expected A=1, got %q", r[1])
 	}
 }
+
+func TestMergeWithEmptyMap(t *testing.T) {
+	r := merge(map[string]string{}, []string{"B=2"})
+
+	if len(r) != 1 {
+		t.Fatalf("expected len(r) = %d, got %d", 1, len(r))
+	}
+
+	if r[0] != "B=2" {
+		t.Errorf("expected B=2, got %q", r[0])
+	}
+}
