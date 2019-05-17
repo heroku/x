f63d2384729586085460e69dfd409eb775eaf031
Andrew G10z <agwozdziewycz@salesforce.com>
2019-05-17 12:11:27 -0700

    Run an actual test against a real redis.

--- hredis/redigo/redigo_test.go
+++ hredis/redigo/redigo_test.go
@@ -2,7 +2,10 @@ package redigo
 
 import (
 	"errors"
+	"net/url"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/gomodule/redigo/redis"
 	"github.com/rafaeljusto/redigomock"
@@ -110,5 +113,68 @@ func TestPoolWithNoGoodPasses(t *testing.T) {
 	c := sut.Get()
 	if c.Err() == nil {
 		t.Fatal("got nil, want err")
+	}
+}
+
+// Note: If this test fails, it can leave the redis with a password different than you
+// expect. Probably best to run redis with a fresh docker container, like so:
+//
+//    docker run --rm -p 127.0.0.1:6379:6379 --name test-redis redis
+//    docker exec -ti test-redis redis-cli config set requirepass password
+//    export REDIS_URL=redis://h:password@127.0.0.1:6379:6379
+func TestForReals(t *testing.T) {
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		t.Skip()
+	}
+
+	u, err := url.Parse(redisURL)
+	if err != nil {
+		t.Fatalf("got %q, want nil", err)
+	}
+
+	initPassword, _ := u.User.Password()
+
+	params := []string{
+		// passwords
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+	}
+
+	sut, err := NewRedisPoolFromURL(redisURL, params...)
+	initConn := sut.Get()
+
+	defer func() {
+		if _, err = initConn.Do("CONFIG", "SET", "requirepass", initPassword); err != nil {
+			t.Fatalf("got %q, want nil", err)
+		}
+	}()
+
+	for _, newPass := range params[1:] {
+		c := sut.Get()
+		if _, err = c.Do("CONFIG", "SET", "requirepass", newPass); err != nil {
+			t.Fatalf("got %q, want nil", err)
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	c := sut.Get()
+	if err != nil {
+		t.Fatalf("got %q, want nil", err)
+	}
+
+	// What's the current pass?
+	if result, err := redis.Strings(c.Do("CONFIG", "GET", "requirepass")); err != nil {
+		if len(result) == 2 {
+			if result[1] == initPassword {
+				t.Errorf("got %s, want NOT %s", result[1], initPassword)
+			}
+		} else {
+			t.Errorf("got %d, want 2", len(result))
+		}
 	}
 }
