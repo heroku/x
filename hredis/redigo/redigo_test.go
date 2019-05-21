dd302f20111894068aeff43cc6429723b63f2e33
Edward Muller <emuller@salesforce.com>
2019-05-20 18:39:13 -0700

    FWIW

--- hredis/redigo/redigo_test.go
+++ hredis/redigo/redigo_test.go
@@ -2,21 +2,17 @@ package redigo
 
 import (
 	"errors"
-	"net/url"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/gomodule/redigo/redis"
 	"github.com/rafaeljusto/redigomock"
 )
 
-func setup(t *testing.T, dialErr error) (*redigomock.Conn, DialURLFunc, func()) {
+func setup(t *testing.T) (*redigomock.Conn, DialURLFunc, func()) {
+	t.Helper()
 	conn := redigomock.NewConn()
 	redisDialURL := func(_ string, options ...redis.DialOption) (redis.Conn, error) {
-		if dialErr != nil {
-			return nil, dialErr
-		}
 		return conn, nil
 	}
 
@@ -25,78 +21,55 @@ func setup(t *testing.T, dialErr error) (*redigomock.Conn, DialURLFunc, func()) 
 	}
 }
 
-func TestPool(t *testing.T) {
-	mock, dialURL, tearDown := setup(t, nil)
+var (
+	errInvalidPassword = errors.New("ERR invalid password")
+)
+
+func TestPoolBadThenGood(t *testing.T) {
+	mock, dialURL, tearDown := setup(t)
 	defer tearDown()
 
-	mock.Command("AUTH", "badpass").ExpectError(errors.New("Bad password"))
-	mock.Command("AUTH", "goodpass")
+	mock.Command("AUTH", "badpass").ExpectError(errInvalidPassword)
+	mock.Command("AUTH", "goodpass").Expect("OK")
 
 	sut, err := NewRedisPoolFromURL("redis://h:badpass@localhost:6379",
 		WithPasswords("goodpass"),
 		WithDialURLFunc(dialURL),
 	)
-
 	if err != nil {
 		t.Fatalf("got %q, want nil", err)
 	}
 
-	// hold on to this conn forever, never close. This forces the next
-	// Get() to redial to get a new connection.
 	c := sut.Get()
 	if c.Err() != nil {
 		t.Fatalf("got %q, want nil", c.Err())
 	}
-
-	mock.Clear()
-
-	// If we get a new connection, only "goodpass" should be called for Auth,
-	// unless that fails again. See "the GoodPassNowFails test"
-	mock.Command("AUTH", "goodpass")
-
-	c = sut.Get()
-	if c.Err() != nil {
-		t.Fatalf("got %q, want nil", c.Err())
-	}
+	c.Close()
 
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Fatalf("got %q, want nil", err)
 	}
 }
 
-func TestPoolGoodPassNowFails(t *testing.T) {
-	mock, dialURL, tearDown := setup(t, nil)
+func TestPoolGoodThenBad(t *testing.T) {
+	mock, dialURL, tearDown := setup(t)
 	defer tearDown()
 
-	mock.Command("AUTH", "badpass").ExpectError(errors.New("Bad password"))
-	mock.Command("AUTH", "goodpass")
+	mock.Command("AUTH", "goodpass").Expect("OK")
 
-	sut, err := NewRedisPoolFromURL("redis://h:badpass@localhost:6379",
-		WithPasswords("goodpass"),
+	sut, err := NewRedisPoolFromURL("redis://h:goodpass@localhost:6379",
+		WithPasswords("badpass"), // won't be used, since goodpass works
 		WithDialURLFunc(dialURL),
 	)
 	if err != nil {
 		t.Fatalf("got %q, want nil", err)
 	}
 
-	// hold on to this conn forever, never close. This forces the next
-	// Get() to redial to get a new connection.
 	c := sut.Get()
 	if c.Err() != nil {
 		t.Fatalf("got %q, want nil", c.Err())
 	}
-
-	mock.Clear()
-
-	// "goodpass" now fails, which is what we used to authenticate successfully before.
-	// But, we should try to auth with "badpass" again, which will now succeed.
-	mock.Command("AUTH", "goodpass").ExpectError(errors.New("Bad Password"))
-	mock.Command("AUTH", "badpass")
-
-	c = sut.Get()
-	if c.Err() != nil {
-		t.Fatalf("got %q, want nil", c.Err())
-	}
+	c.Close()
 
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Fatalf("got %q, want nil", err)
@@ -104,11 +77,11 @@ func TestPoolGoodPassNowFails(t *testing.T) {
 }
 
 func TestPoolWithNoGoodPasses(t *testing.T) {
-	mock, dialURL, tearDown := setup(t, nil)
+	mock, dialURL, tearDown := setup(t)
 	defer tearDown()
 
-	mock.Command("AUTH", "badpass").ExpectError(errors.New("Bad password"))
-	mock.Command("AUTH", "alsobadpass").ExpectError(errors.New("Bad password"))
+	mock.Command("AUTH", "badpass").ExpectError(errInvalidPassword)
+	mock.Command("AUTH", "alsobadpass").ExpectError(errInvalidPassword)
 
 	sut, err := NewRedisPoolFromURL("redis://h:badpass@localhost:6379",
 		WithPasswords("alsobadpass"),
@@ -124,28 +97,40 @@ func TestPoolWithNoGoodPasses(t *testing.T) {
 	if c.Err() == nil {
 		t.Fatal("got nil, want err")
 	}
+	c.Close()
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatalf("got %q, want nil", err)
+	}
+
 }
 
 // Note: If this test fails, it can leave the redis with a password different than you
 // expect. Probably best to run redis with a fresh docker container, like so:
 //
 //    docker run --rm -p 127.0.0.1:6379:6379 --name test-redis redis
-//    docker exec -ti test-redis redis-cli config set requirepass password
-//    export REDIS_URL=redis://h:password@127.0.0.1:6379:6379
-func TestForReals(t *testing.T) {
+//    REDIS_URL=redis://127.0.0.1:6379 go test -count=1 -v ./hredis/...
+func TestIntegrationTest(t *testing.T) {
 	redisURL := os.Getenv("REDIS_URL")
 	if redisURL == "" {
 		t.Skip()
 	}
 
-	u, err := url.Parse(redisURL)
+	mp, err := NewRedisPoolFromURL(redisURL)
 	if err != nil {
-		t.Fatalf("got %q, want nil", err)
+		t.Fatal("expected nil, got ", err)
+	}
+	defer mp.Close()
+
+	mconn := mp.Get()
+	defer mconn.Close()
+
+	_, err = mconn.Do("PING")
+	if err != nil {
+		t.Fatal("expected nil, got ", err)
 	}
 
-	initPassword, _ := u.User.Password()
-
-	passes := []string{
+	passwords := []string{
 		"one",
 		"two",
 		"three",
@@ -153,37 +138,36 @@ func TestForReals(t *testing.T) {
 		"five",
 	}
 
-	sut, err := NewRedisPoolFromURL(redisURL, WithPasswords(passes...))
-	initConn := sut.Get()
+	var cpwd string
+	for _, pwd := range passwords {
+		t.Run(pwd, func(t *testing.T) {
+			if cpwd != "" {
+				if _, err := mconn.Do("AUTH", cpwd); err != nil {
+					t.Fatal("expected nil, got ", err)
+				}
+			}
+			if _, err := mconn.Do("CONFIG", "SET", "requirepass", pwd); err != nil {
+				t.Fatal("expected nil, got ", err)
+			}
+			cpwd = pwd
+
+			p, err := NewRedisPoolFromURL(redisURL, WithPasswords(passwords...))
+			if err != nil {
+				t.Fatal("expected nil, got ", err)
+			}
+
+			c := p.Get()
+			if _, err := c.Do("PING"); err != nil {
+				t.Fatal("expected nil, got ", err)
+			}
+			c.Close()
+		})
+	}
 
 	defer func() {
-		if _, err = initConn.Do("CONFIG", "SET", "requirepass", initPassword); err != nil {
-			t.Fatalf("got %q, want nil", err)
+		// reset the password to none
+		if _, err := mconn.Do("CONFIG", "SET", "requirepass", ""); err != nil {
+			t.Fatal("expected nil, got ", err)
 		}
 	}()
-
-	for _, newPass := range passes {
-		c := sut.Get()
-		if _, err = c.Do("CONFIG", "SET", "requirepass", newPass); err != nil {
-			t.Fatalf("got %q, want nil", err)
-		}
-
-		time.Sleep(10 * time.Millisecond)
-	}
-
-	c := sut.Get()
-	if err != nil {
-		t.Fatalf("got %q, want nil", err)
-	}
-
-	// What's the current pass?
-	if result, err := redis.Strings(c.Do("CONFIG", "GET", "requirepass")); err != nil {
-		if len(result) == 2 {
-			if result[1] == initPassword {
-				t.Errorf("got %s, want NOT %s", result[1], initPassword)
-			}
-		} else {
-			t.Errorf("got %d, want 2", len(result))
-		}
-	}
 }
