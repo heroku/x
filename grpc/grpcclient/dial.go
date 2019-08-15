122f8ea2ddca0db70d3a4fd998d9b24b5b49d79e
Dane Harrigan <dane@heroku.com>
2019-08-15 16:56:01 -0700

    Add AppendOutgoingRequestID gRPC client interceptor
    When added to a gRPC dialer, if the context contains a Request-ID, the value
    will be extracted and appended to the outgoing context.
    
    Appending Request-IDs to the outgoing context will enable gRPC clients, to
    forward the Request-IDs to their respective services. Ultimately tracking a
    request across multiple services.

--- grpc/grpcclient/dial.go
+++ grpc/grpcclient/dial.go
@@ -1,8 +1,10 @@
 package grpcclient
 
 import (
+	"context"
 	"net/url"
 
+	"github.com/heroku/x/grpc/requestid"
 	"github.com/heroku/x/tlsconfig"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -22,4 +24,14 @@ func Credentials(serverURL string, caCerts [][]byte, clientCert, clientKey []byt
 	cfg.ServerName = uri.Host
 
 	return grpc.WithTransportCredentials(credentials.NewTLS(cfg)), nil
+}
+
+// AppendOutgoingRequestID reads the incoming Request-ID from the context and appends it to the
+// outgoing context. Forwarding Request-IDs from gRPC service to service allows for request
+// tracking across any number of services.
+func AppendOutgoingRequestID() grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		ctx = requestid.AppendToOutgoingContext(ctx)
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
 }
