7ceaf85171b08f56558927100642e19a97bfe01d
Jillian Tullo <jtullo@heroku.com>
2019-09-10 11:33:03 -0400

    Add example for mustcert

---
+++ testing/mustcert/README.md
@@ -0,0 +1,5 @@
+# mustcert 
+mustcert is a package that creates certificates for testing TLS.
+
+## Documentation
+Full documentation is available on [godoc](https://godoc.org/github.com/heroku/x/testing/mustcert).
---
+++ testing/mustcert/example_test.go
@@ -0,0 +1,73 @@
+package mustcert_test
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"io/ioutil"
+
+	"github.com/heroku/dogwood/pkg/mustcert"
+)
+
+// This example uses mustcert to create certificates, start a TLS server, and a client to
+// talk to it.
+func Example() {
+	ca := mustcert.CA("root", nil)
+	serverCert := mustcert.Leaf("localhost", ca)
+	clientCert := mustcert.Leaf("client", ca)
+
+	// Create the TLS Test Server
+	server := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello, world!"))
+	}))
+
+	rootCAs := mustcert.Pool(ca.TLS())
+	server.TLS = &tls.Config{
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{*serverCert.TLS()},
+		RootCAs: rootCAs,
+		ClientCAs: rootCAs,
+	}
+	server.StartTLS()
+	defer server.Close()
+
+	// Create the Client configuration
+	cert, err := tls.X509KeyPair([]byte(clientCert.CertPEM()), []byte(clientCert.KeyPEM()))
+	if err != nil {
+		fmt.Println(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM([]byte(ca.CertPEM()))
+	config := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs: caCertPool,
+		InsecureSkipVerify: true,
+	}
+	config.BuildNameToCertificate()
+
+	// Create the HTTP Client
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: config,
+		},
+	}
+
+	// Make a client request to the HTTP Server
+	resp, err := client.Get(server.URL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
+
+	// Output:
+	// hello, world!
+}
