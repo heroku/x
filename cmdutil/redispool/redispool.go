adfb4f8ff884e61f9577ed46489303ca65ec943b
Tilman Holschuh <tilman@heroku.com>
2019-10-31 09:58:04 -0700

    Add optional config to set max conn lifetime

--- cmdutil/redispool/redispool.go
+++ cmdutil/redispool/redispool.go
@@ -13,6 +13,7 @@ type Config struct {
 	URL               string        `env:"REDIS_URL,default=redis://127.0.0.1:6379"`
 	MaxIdleConns      int           `env:"REDIS_MAX_IDLE_CONNS,default=1"`
 	MaxActiveConns    int           `env:"REDIS_MAX_ACTIVE_CONNS,default=50"`
+	MaxConnLifetime   time.Duration `env:"REDIS_MAX_CONN_LIFETIME,default=0s"`
 	IdleTimeout       time.Duration `env:"REDIS_IDLE_TIMEOUT,default=0s"`
 	IdleProbeInterval time.Duration `env:"REDIS_IDLE_PROBE_INTERVAL,default=0s"`
 	AttachmentNames   []string      `env:"REDIS_ATTACHMENT_NAMES"`
@@ -48,9 +49,10 @@ func newPool(cfg Config) *redis.Pool {
 			}
 			return conn, nil
 		},
-		MaxIdle:     cfg.MaxIdleConns,
-		MaxActive:   cfg.MaxActiveConns,
-		IdleTimeout: cfg.IdleTimeout,
+		MaxIdle:         cfg.MaxIdleConns,
+		MaxActive:       cfg.MaxActiveConns,
+		MaxConnLifetime: cfg.MaxConnLifetime,
+		IdleTimeout:     cfg.IdleTimeout,
 		TestOnBorrow: func(conn redis.Conn, t time.Time) error {
 			var err error
 			if cfg.IdleProbeInterval > 0 && time.Since(t) > cfg.IdleProbeInterval {
