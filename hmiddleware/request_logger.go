e2c7972a79236ca37e0343e8a021c2ecaa8209b9
Jessie A. Young <jessieay@users.noreply.github.com>
2020-01-27 14:35:22 -0800

    Re-work logging middleware to be compatible with Chi (#118)
    * Previously, I added logging middleware that coudld be used by the Chi
    router but did not interact with Chi.
    * When we were testing our rollbar setup in Lyra Identity, we found that
    the Panic recovery was not working within Chi router endpoints.
    * This was because the Recovery middleware depends on the Logging
    middleware that Chi provides, which we were not using because we were
    using this handy Logging middleware from X instead.
    * At this point, it seemed like a better solution than writing our own
    recovery middleware for Chi was to implement the LogFormatter and
    LogEntry interfaces so we can use the Chi middleware for logging and
    panic recovery.
    * This is the approach shown here https://github.com/go-chi/chi/blob/cca4135d8dddff765463feaf1118047a9e506b4a/_examples/logging/main.go
    * Also used by Lyra Labs here https://github.com/heroku/lyra-labs/pull/212/
    * Logging has been adapted to use the fields that other Heroku services
    use for easier Spluking etc.

---
+++ hmiddleware/request_logger.go
@@ -0,0 +1,74 @@
+package hmiddleware
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/pkg/errors"
+
+	"github.com/heroku/x/requestid"
+
+	"github.com/sirupsen/logrus"
+)
+
+// StructuredLogger implements the LogFormatter interface from Chi.
+// LogFormatter initiates the beginning of a new LogEntry per request.
+// See https://github.com/go-chi/chi/blob/708d187cdc2beff37b6835250dd574f395ebaa03/middleware/logger.go#L54
+// For an example of how to use this middleware to combine Logrus logging with the Chi router, see
+// https://github.com/go-chi/chi/blob/cca4135d8dddff765463feaf1118047a9e506b4a/_examples/logging/main.go#L2
+type StructuredLogger struct {
+	Logger logrus.FieldLogger
+}
+
+// StructuredLoggerEntry implements the LogEntry interface from Chi.
+// LogEntry records the final log when a request completes.
+// See https://github.com/go-chi/chi/blob/708d187cdc2beff37b6835250dd574f395ebaa03/middleware/logger.go#L60
+// For an example of how to use this middleware to combine Logrus logging with the Chi router, see
+// https://github.com/go-chi/chi/blob/cca4135d8dddff765463feaf1118047a9e506b4a/_examples/logging/main.go#L2
+type StructuredLoggerEntry struct {
+	Logger logrus.FieldLogger
+}
+
+// NewLogEntry creates a new LogEntry at the start of a request.
+func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
+	logFields := logrus.Fields{
+		"request_id":  requestid.Get(r),
+		"method":      r.Method,
+		"host":        r.Host,
+		"path":        r.URL.RequestURI(),
+		"remote_addr": r.RemoteAddr,
+		"user_agent":  r.UserAgent(),
+		"protocol":    r.URL.Scheme,
+		"at":          "start",
+	}
+
+	if robot := r.Header.Get("X-Heroku-Robot"); robot != "" {
+		logFields["robot"] = robot
+	}
+
+	log := l.Logger.WithFields(logFields)
+	log.Info()
+
+	return &StructuredLoggerEntry{Logger: log}
+}
+
+// Write creates a new LogEntry at the end of a request.
+func (l *StructuredLoggerEntry) Write(status, bytes int, elapsed time.Duration) {
+	l.Logger.WithFields(logrus.Fields{
+		"at":      "finish",
+		"status":  status,
+		"bytes":   bytes,
+		"service": fmt.Sprintf("%dms", elapsed/time.Millisecond),
+	}).Info()
+}
+
+// Panic is called by Chi's Recoverer middleware.
+// See https://github.com/go-chi/chi/blob/baf4ef5b139e284b297573d89daf587457153aa3/middleware/recoverer.go
+func (l *StructuredLoggerEntry) Panic(v interface{}, stack []byte) {
+	werr := errors.Errorf("panic: %v", v)
+	l.Logger.WithFields(logrus.Fields{
+		"stack": string(stack),
+	}).WithError(werr).Error("unhandled panic")
+}
---
+++ hmiddleware/request_logger_test.go
@@ -0,0 +1,63 @@
+package hmiddleware
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/heroku/x/testing/testlog"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestNewLogEntry(t *testing.T) {
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set("X-Heroku-Robot", "I am a robot")
+	logger := logrus.New()
+	l := &StructuredLogger{
+		Logger: logger,
+	}
+
+	e := l.NewLogEntry(req)
+	if e == nil {
+		t.Error("error creating NewLogEntry.")
+	}
+}
+
+func TestLogEntryWrite(t *testing.T) {
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
+	logger, hook := testlog.NewNullLogger()
+	l := &StructuredLogger{
+		Logger: logger,
+	}
+	e := l.NewLogEntry(req)
+	status := 200
+	bytes := 0
+	elapsed := time.Duration(100)
+
+	e.Write(status, bytes, elapsed)
+
+	hook.CheckContained(t,
+		`level="info"`,
+		`at="finished"`,
+		`status="200"`,
+	)
+}
+
+func TestLogEntryPanic(t *testing.T) {
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
+	logger, hook := testlog.NewNullLogger()
+	l := &StructuredLogger{
+		Logger: logger,
+	}
+	e := l.NewLogEntry(req)
+
+	var i interface{}
+	b := []byte{65, 66}
+	e.Panic(&i, b)
+
+	hook.CheckContained(t,
+		`level="error"`,
+		`msg="unhandled panic"`,
+	)
+}
