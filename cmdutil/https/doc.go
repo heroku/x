d829bdc367e5210ef0702da8ef7da1d957bf3c29
Bernerd Schaefer <bj.schaefer@gmail.com>
2020-02-18 09:12:55 -0800

    SSL->TLS

--- cmdutil/https/doc.go
+++ cmdutil/https/doc.go
@@ -1,2 +1,2 @@
-// Package https provides utilities for configuring and running HTTP servers over SSL.
+// Package https provides utilities for configuring and running HTTP servers over TLS.
 package https
