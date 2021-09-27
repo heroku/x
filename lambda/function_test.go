1f9de089b2d6d8ba17f2c6b81700562cc96bb06c
slizco <teamslizco@gmail.com>
2021-09-27 10:55:52 -0400

    Add test for creating new function

---
+++ lambda/function_test.go
@@ -0,0 +1,30 @@
+package lambda
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNew_Function(t *testing.T) {
+	setupStandardConfig(t)
+
+	f := New(nil)
+
+	if f.Logger == nil {
+		t.Fatal("logger not configured")
+	}
+
+	if f.MetricsProvider == nil {
+		t.Fatal("metrics provider not configured")
+	}
+}
+
+func setupStandardConfig(t *testing.T) {
+	os.Setenv("APP_NAME", "test-app")
+	os.Setenv("DEPLOY", "test")
+
+	t.Cleanup(func() {
+		os.Unsetenv("APP_NAME")
+		os.Unsetenv("DEPLOY")
+	})
+}
