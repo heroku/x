7f0dca6ed104a7606c11688e38ef26ef60df397e
slizco <teamslizco@gmail.com>
2021-11-17 08:31:54 -0500

    Fix env var tag fmt

--- cmdutil/metrics/otel/otel.go
+++ cmdutil/metrics/otel/otel.go
@@ -18,7 +18,7 @@ import (
 // Config is a reusable configuration struct that contains the necessary
 // environment variables to setup an metrics.Provider
 type Config struct {
-	Enabled             bool     `env:"ENABLE__COLLECTION"`
+	Enabled             bool     `env:"ENABLE_OTEL_COLLECTION"`
 	CollectorURL        *url.URL `env:"OTEL_COLLECTOR_URL"`
 	MetricsDestinations []string `env:"OTEL_METRICS_DESTINATIONS,default=honeycomb,argus"`
 	Honeycomb           honeycomb.Config
