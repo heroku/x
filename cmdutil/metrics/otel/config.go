8276291a61287897fe7d958d19f1c7954c739eac
Alex Arnell <alex.arnell@gmail.com>
2022-01-26 09:43:33 -0800

    resolve golangci-lint issues

--- cmdutil/metrics/otel/config.go
+++ cmdutil/metrics/otel/config.go
@@ -9,9 +9,9 @@ import (
 // Config is a reusable configuration struct that contains the necessary
 // environment variables to setup an metrics.Provider
 type Config struct {
-	Enabled             bool     `env:"ENABLE_OTEL_COLLECTION"`
-	CollectorURL        *url.URL `env:"OTEL_COLLECTOR_URL"`
-	UseExactAggregator  bool     `env:"OTEL_USE_EXACT_AGGREGATOR"`
 	MetricsDestinations []string `env:"OTEL_METRICS_DESTINATIONS,default=honeycomb;argus"`
 	Honeycomb           honeycomb.Config
+	CollectorURL        *url.URL `env:"OTEL_COLLECTOR_URL"`
+	Enabled             bool     `env:"ENABLE_OTEL_COLLECTION"`
+	UseExactAggregator  bool     `env:"OTEL_USE_EXACT_AGGREGATOR"`
 }
