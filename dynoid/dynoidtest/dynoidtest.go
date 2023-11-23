ffca2c1cb6a1a1a2c9818c1ff579406a4ad784e4
Troels Thomsen <troels@thomsen.io>
2023-11-23 11:18:54 +0100

    Use `http.ServeMux` to reduce dependencies

--- dynoid/dynoidtest/dynoidtest.go
+++ dynoid/dynoidtest/dynoidtest.go
@@ -9,7 +9,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/go-chi/chi"
 	"github.com/golang-jwt/jwt/v4"
 	jose "gopkg.in/square/go-jose.v2"
 )
@@ -55,9 +54,9 @@ type roundTripper struct {
 }
 
 func (rt *roundTripper) init() {
-	r := chi.NewRouter()
+	mux := http.NewServeMux()
 
-	r.Get("/issuers/test/.well-known/openid-configuration", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/issuers/test/.well-known/openid-configuration", func(w http.ResponseWriter, r *http.Request) {
 		header := w.Header()
 		header.Set("Content-Type", "application/json")
 
@@ -74,7 +73,7 @@ func (rt *roundTripper) init() {
 			`}`))
 	})
 
-	r.Get("/issuers/test/.well-known/jwks.json", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/issuers/test/.well-known/jwks.json", func(w http.ResponseWriter, r *http.Request) {
 		jwks := &jose.JSONWebKeySet{}
 		jwks.Keys = append(jwks.Keys, jose.JSONWebKey{Key: rt.issuer.key.Public(), KeyID: "primary"})
 
@@ -87,7 +86,7 @@ func (rt *roundTripper) init() {
 		_ = enc.Encode(jwks)
 	})
 
-	rt.handler = r
+	rt.handler = mux
 }
 
 func (rt *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
