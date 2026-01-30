9e72f17e09e59c42e3b0f8409b54e37809692972
Tilman Holschuh <tholschuh@salesforce.com>
2026-01-30 12:59:18 -0800

    Disable otel metrics by default

--- cmdutil/v2/metrics/config.go
+++ cmdutil/v2/metrics/config.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Config struct {
-	Enabled              bool          `env:"OTEL_METRICS_ENABLED,default=true"`
+	Enabled              bool          `env:"OTEL_METRICS_ENABLED,default=false"`
 	Endpoint             *url.URL      `env:"OTEL_EXPORTER_OTLP_ENDPOINT"`
 	Protocol             string        `env:"OTEL_EXPORTER_OTLP_PROTOCOL,default=http/protobuf"`
 	Interval             time.Duration `env:"OTEL_METRIC_EXPORT_INTERVAL,default=60s"`
